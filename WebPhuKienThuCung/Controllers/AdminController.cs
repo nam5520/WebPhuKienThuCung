e44d16595bdec93f30f958dc8eed8adadd01c98c
ASUS <ASUS@DESKTOP-95UUH52>
2023-03-03 00:51:32 +0700

    Add project files.

---
+++ WebPhuKienThuCung/Controllers/AdminController.cs
@@ -0,0 +1,71 @@
+﻿using Antlr.Runtime.Misc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebPhuKienThuCung.Models;
+
+namespace WebPhuKienThuCung.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        QLPKTHUCUNGEntities1 db = new QLPKTHUCUNGEntities1();
+        public ActionResult Index()
+        {
+            if (Session["Taikhoanadmin"] == null)
+                return RedirectToAction("dangnhap", "Admin");
+            else
+            {
+                ViewBag.Trang = "Trang chủ"; 
+                return View();
+            }
+            
+        }
+        [HttpGet]
+        public ActionResult dangnhap()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult dangnhap(DangNhapModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ADMIN ad = db.ADMINs.SingleOrDefault(n => n.TENDN == model.tendn && n.MATKHAU == model.matkhau);
+                if (ad != null)
+                {
+                    ViewBag.Thongbao = "Đăng nhập thành công";
+                    Session["Taikhoanadmin"] = ad;
+                    return RedirectToAction("Index", "Admin");
+                }
+                else
+                    ViewBag.Thongbao = "Tên đăng nhập hoặc mật khẩu không đúng";
+            }
+            else
+            {
+
+            }
+            return View(model);
+        }
+        public ActionResult dangxuat()
+        {
+            Session.Clear();
+            return RedirectToAction("index","Admin");
+        }
+        public ActionResult listadmin()
+        {
+            if (Session["Taikhoanadmin"] == null)
+            {
+                return RedirectToAction("dangnhap", "Admin");
+
+            }
+            else
+            {
+                var ad = from admin in db.ADMINs select admin;
+                return View(ad);
+            }
+        }
+    }
+}
---
+++ WebPhuKienThuCung/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WebPhuKienThuCung")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebPhuKienThuCung")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ab23ba01-912d-4e22-a8dc-e484c3d56b70")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
